e74e286b354341d9ab7a8ef212041e2ed6c4ae84
ken-1200 <kensuke19971022@gmail.com>
2021-06-24 22:13:51 +0900

    feat: 新規タスクフォーム追加

--- src/components/NewTask.tsx
+++ src/components/NewTask.tsx
@@ -1,24 +1,89 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-// import { Link } from 'react-router-dom';
+import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
 
-import {  } from '../actions';
+import { createTask } from '../actions';
 import '../App.css';
 
-type Props = any;
+type Props = {
+  createTask: any;
+  history: any;
+}
+
+type submitType = {
+  title: string;
+  text: string;
+}
 
 class NewTask extends Component<Props, {}> {
+  // 初期化
+  constructor(props: any) {
+    super(props);
+
+    // イベントハンドラをインスタンスにバインドする
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  // メンバ関数
+  private renderField(field: any): JSX.Element {
+    const {
+      input,
+      label,
+      type,
+      meta: { touched, error }
+    }
+    = field;
+
+    return (
+      <div>
+        <input { ...input } placeholder={ label } type={ type } />
+        { touched && error && <span>{ error }</span> }
+      </div>
+    )
+  }
+
+  private async onSubmit(values: submitType) {
+    await this.props.createTask(values);
+
+    // task一覧画面へ
+    this.props.history.push("/");
+  }
+
   // 画面描画
   render() {
+    const { handleSubmit }: any = this.props;
+
     return (
       <React.Fragment>
-        <div>new task</div>
+        <form onSubmit={ handleSubmit(this.onSubmit) }>
+          <div>
+            <Field label="Title" name="title" type="text" component={ this.renderField } />
+            <Field label="Text" name="text" type="text" component={ this.renderField } />
+          </div>
+
+          <div>
+            <input type="submit" value="Submit" disabled={ false } />
+            <Link to="/">Cancel</Link>
+          </div>
+        </form>
       </React.Fragment>
     );
   };
 };
 
-// ステートとアクション(reducersの中身)をPropsに渡す
-// const mapDispathToProps = ({ });
+const validate = (values: any) => {
+  const error: any = {};
 
-export default connect(null, null)(NewTask);
+  if (!values.title) error.title = "タイトルを入力してください。";
+  if (!values.text) error.text = "テキストを入力してください。";
+
+  return error;
+}
+
+// ステートとアクション(reducersの中身)をPropsに渡す
+const mapDispathToProps: any = ({ createTask });
+
+export default connect(null, mapDispathToProps)(
+  reduxForm<{}, any>({ validate, form: "newTaskForm" })(NewTask)
+);
